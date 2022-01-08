4f167328702f9704af2df2279881366a5b4e44b4
Olayinka Okewale <okjool2012@gmail.com>
2022-01-09 00:44:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Script to Create RVE with Damask
+Create several RVE grids with damask
+
+## Prerequisites
+1. Python 3.8
+2. [Damask](https://damask.mpie.de/installation/index.html)
+
+## How to run
+1. Open `create_rves.py`
+2. Update the loop range to desireable grain number and increment
+3. Run the script using: `python3 create_rves.py`
---
+++ create_rves.py
@@ -0,0 +1,35 @@
+import sys
+sys.path.append('/nethome/storage/raid2/o.okewale/damask/python')
+# print(sys.path)
+import os
+import damask
+import numpy
+
+def createDirectory(dir:str):
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+def createRVE(start:int, grains:int, path:str='rves/'):
+    size = numpy.ones(3)*1e-5
+    cells = [pow(2,start),pow(2,start),pow(2,start)]
+    N_grains = grains
+    
+
+    # Create the first RVE with grain
+    seeds = damask.seeds.from_random(size, N_grains, cells)
+    grid = damask.Grid.from_Voronoi_tessellation(cells,size,seeds)
+    grid.save(f'{path}Polycystal_{N_grains}_{cells[0]}x{cells[1]}x{cells[2]}')
+
+    # Rescale the previously created grid
+    for x in range(start-1,3,-1):
+        new_cells = [pow(2,x), pow(2,x), pow(2,x)]
+        output_file = f'{path}Polycystal_{N_grains}_{new_cells[0]}x{new_cells[1]}x{new_cells[2]}'
+        scaled = grid.scale(new_cells)
+        scaled.save(output_file)
+
+
+# loop through range of grains and increment by 20
+for y in range(20,200,20):
+    path = f'rves/{y}_grains/'
+    createDirectory(path)
+    createRVE(7,y,path)
