857efaf92e2d08b486ac546f1a7a4288298d9dbd
Olayinka Okewale <okjool2012@gmail.com>
2022-01-13 00:30:06 +0100

    changes made to volume calculation

--- Different_grains_same_resolution.py
+++ Different_grains_same_resolution.py
@@ -11,24 +11,28 @@ d = 32E-06    # diameter of the grain
 
 r = d/2.0
 
-grain_vol = (4.0/3.0)*np.pi*(r**3.0) # vol of a grain
+grain_vol = (4.0/3.0)*np.pi*(r**(1.0/3.0)) # vol of a grain
 
 cells_per_grain = grain_vol/dx    # need to keep this number constant
 
 cells_per_grain_int = round(cells_per_grain)  # rounding it off
+print ("Cells per grain =>", cells_per_grain_int)
 
 # cells for the geometry
 
-number_of_grains = 20
+number_of_grains = 60
 
-total_cells_needed = 20*cells_per_grain_int
+total_cells_needed = number_of_grains*cells_per_grain_int
 
 cells_x = round((total_cells_needed)**(1.0/3.0))
 
 cells = np.array([cells_x,cells_x,cells_x])
 size = cells*dx
 
+print("Cells =>", cells)
+print("Size =>", size)
+
 # creating the actual geometry
 seeds = seeds.from_random(size,number_of_grains,cells)
 grid  = Grid.from_Voronoi_tessellation(cells,size,seeds,periodic=True)    #always make periodic
-grid.save(f'Polycrystal_{number_of_grains}_{cells[0]}x{cells[1]}x{cells[2]}')
+grid.save(f'rves/Polycrystal_{number_of_grains}_{cells[0]}x{cells[1]}x{cells[2]}')
